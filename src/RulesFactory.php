c76d56eb101a61e38c8e44c2f01c1c3d5f8bff29
Rob Waller <rdwaller1984@gmail.com>
2021-08-10 15:03:30 +0100

    Create RulesFactory tests to generate a collection of Rules based off the rule number.

---
+++ src/RulesFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Automata;
+
+use Automata\Rules;
+use Automata\Rule;
+
+class RulesFactory
+{
+    private const CELL_RULE = [
+        "111",
+        "110",
+        "101",
+        "100",
+        "001",
+        "010",
+        "001",
+        "000",
+    ];
+
+    public function create(int $rule): Rules
+    {
+        $binaryRule = str_pad(base_convert((string) $rule, 10, 2), 8, "0", STR_PAD_LEFT);
+
+        $rules = new Rules();
+
+        $i = 0;
+
+        foreach (str_split($binaryRule) as $cellState) {
+            $rules->add(new Rule(self::CELL_RULE[$i], (int) $cellState));
+            $i++;
+        }
+
+        return $rules;
+    }
+}
