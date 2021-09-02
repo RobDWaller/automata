4950fd7f8c7e468e0a9e4217b33e5768219674c1
Rob Waller <rdwaller1984@gmail.com>
2021-09-02 17:32:18 +0100

    Created new iterate test for Iterator class which generates a collection of iterations for cells at each time step.

--- src/Iterator.php
+++ src/Iterator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Automata;
 
 use Automata\Iterate;
+use Automata\Iterations;
 use Automata\Cells;
 use Automata\Rules;
 
@@ -15,6 +16,21 @@ class Iterator
         private Cells $cells,
         private Rules $rules
     ) {
+    }
+
+    public function iterate(int $steps): Iterations
+    {
+        $count = 0;
+        $iterations = new Iterations();
+        $iterations->add($this->cells);
+
+        while ($count < $steps) {
+            $this->cells = $this->iterate->step($this->cells, $this->rules);
+            $iterations->add($this->cells);
+            $count++;
+        }
+
+        return $iterations;
     }
 
     public function iterateTo(int $steps): Cells
--- tests/IteratorTest.php
+++ tests/IteratorTest.php
@@ -29,4 +29,24 @@ class IteratorTest extends TestCase
         $this->assertSame(1, $result->find(3)->getState());
         $this->assertSame(0, $result->find(4)->getState());
     }
+
+    public function testIterate(): void
+    {
+        $cellsFactory = new CellsFactory();
+        $cells = $cellsFactory->create("01010");
+
+        $rulesFactory = new RulesFactory();
+        $rules = $rulesFactory->create(110);
+
+        $iterator = new Iterator(new Iterate(), $cells, $rules);
+
+        $result = $iterator->iterate(4);
+
+        $this->assertCount(5, $result);
+        $this->assertSame(1, $result->find(3)->find(0)->getState());
+        $this->assertSame(0, $result->find(3)->find(1)->getState());
+        $this->assertSame(1, $result->find(3)->find(2)->getState());
+        $this->assertSame(1, $result->find(3)->find(3)->getState());
+        $this->assertSame(0, $result->find(3)->find(4)->getState());
+    }
 }
